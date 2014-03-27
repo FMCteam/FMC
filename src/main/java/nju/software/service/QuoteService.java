d723c4e71b63f36124f40e438467d215637fa36a
wangliming1992 <wangliming1992@gmail.com>
2014-03-27 16:44:15 +0800

    modify quote

--- src/main/java/nju/software/service/QuoteService.java
+++ src/main/java/nju/software/service/QuoteService.java
@@ -1,6 +1,10 @@
 package nju.software.service;
 
+import nju.software.dataobject.Quote;
+
 public interface QuoteService {
 
 	public boolean updateQuote(float innerPrice,float outPrice,int id,long taskId,long processId,String actor);
+
+	public Quote findByOrderId(String orderId);
 }
