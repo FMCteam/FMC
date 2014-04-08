7eb9b521d46c0c5184f65fa193929c84c8f370eb
NewHy2014 <1067575915@qq.com>
2014-04-08 17:50:49 +0800

    HYEdit

--- doc/sql/Dump20140303副本.sql
+++ doc/sql/Dump20140303副本.sql
@@ -1,29 +1,4 @@
-CREATE DATABASE  IF NOT EXISTS `fmc_test` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `fmc_test`;
--- MySQL dump 10.13  Distrib 5.6.13, for osx10.6 (i386)
---
--- Host: 192.168.55.52    Database: fmc
--- ------------------------------------------------------
--- Server version	5.6.16
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `accessory`
---
-
-DROP TABLE IF EXISTS `accessory`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `accessory` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `order_id` int(11) NOT NULL,
