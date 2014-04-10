2913282761cd26502dc3ea8c21e82a8118cb3a48
unknown <524262909@qq.com>
2014-04-10 17:10:19 +0800

    update

--- src/main/webapp/js/fmc/table.js
+++ src/main/webapp/js/fmc/table.js
@@ -5,8 +5,12 @@ $(function() {
 				if ($(this).val() != "") {
 					$("table tbody tr").hide().filter(
 							":contains('" + ($(this).val()) + "')").show();
+					var i=$("table tbody tr:not(:hidden)").length;
+					$("table.list span.number").text(i);
 				}else{
 					$("table tbody tr").show();
+					var i=$("table tbody tr:not(hidden)").length;
+					$("table.list span.number").text(i);
 				}
 			});
 
