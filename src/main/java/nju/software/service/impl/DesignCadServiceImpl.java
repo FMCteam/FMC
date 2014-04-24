ac70b0cc7c0cc39a2b79dd7719331241e0f04806
wangliming1992 <wangliming1992@gmail.com>
2014-04-24 17:15:16 +0800

    yf

---
+++ src/main/java/nju/software/service/impl/DesignCadServiceImpl.java
@@ -0,0 +1,25 @@
+package nju.software.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import nju.software.dao.impl.DesignCadDAO;
+import nju.software.dataobject.DesignCad;
+import nju.software.service.DesignCadService;
+
+public class DesignCadServiceImpl implements DesignCadService{
+
+	@Autowired
+	private DesignCadDAO cadDAO;
+	@Override
+	public DesignCad findByOrderId(String orderId) {
+		// TODO Auto-generated method stub
+		List<DesignCad> list = cadDAO.findByOrderId(orderId);
+		if(list.size()>0)
+			return list.get(0);
+		else
+			return null;
+	}
+
+}
