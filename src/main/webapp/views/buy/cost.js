615e54cf5780fe2be49df3dfe1d8f2a75d67df44
ukiyoe <wj.ukiyoe@gmail.com>
2014-05-20 17:38:41 +0800

    buy

--- src/main/webapp/views/buy/cost.js
+++ src/main/webapp/views/buy/cost.js
@@ -3,24 +3,18 @@ jQuery(document).ready(function(){
 		
 		
 		
-		
-		if(checkIsNumber("fabric_amount")&&checkIsNumber("tear_per_meter")
-				&&checkIsNumber("cost_per_meter")&&checkIsNumber("tear_per_piece")
-				&&checkIsNumber("cost_per_piece")){
-				return confirm("确认操作？");
-			}else{
-				alert("面辅所有信息必须填写，其中面料克重、单件米耗、每米价格、单件耗数、辅料单价必须是数字");
-				return false;
-			}
-		
-		
-		
-		function checkIsNumber(name){
-			return !isNaN($("input[name='"+name+"']").val())&&($("input[name='"+name+"']").val()!="");
-		}
-		
-		
-		
+		 if((FabricName== "")||(FabricAmount== "")||(TearPerMeter== "")||(CostPerMeter== "")||(AccessoryName== "")
+				 ||(AccessoryQuery== "")||(TearPerPiece== "")||(CostPerPiece== "")){
+			 
+			alert("请将面辅信息填写完整");
+			  return false;
+			
+		 }
+			
+		 else {
+			 
+			 return confirm("确认操作？");
+		 }
 	
 		
 		
