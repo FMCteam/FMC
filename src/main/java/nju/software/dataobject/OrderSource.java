e665695b50d0ff940143f3b829d259ab0debed57
margine <ch_margine@163.com>
2015-04-17 18:16:38 +0800

    1、市场专员界面添加了认领客户新单选项 2、添加了OrderSource（订单生成来源）的DAO和dataObject

---
+++ src/main/java/nju/software/dataobject/OrderSource.java
@@ -0,0 +1,74 @@
+package nju.software.dataobject;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 专员订单的来源方式，分为专员下单，客户指定专员下单，市场秘书分配的订单，专员认领的订单，市场主管更换专员后被分配的订单五种
+ * @author margine
+ *
+ */
+@Entity
+@Table(name = "order_source", catalog = "fmc")
+public class OrderSource implements Serializable{
+	
+	/**来自专员自己下的订单*/
+	public static final String SOURCE_SELF = "Self";
+	/**来自客户指定专员的订单*/
+	public static final String SOURCE_CUSTOMER = "Customer";
+	/**来自市场秘书分配的订单*/
+	public static final String SOURCE_ALLOCATE ="Allocate";
+	/**专员认领的客户订单*/
+	public static final String SOURCE_CALIM = "Claim";
+	/**来自市场主管同意变更申请后，指定专员的订单*/
+	public static final String SOURCE_ALTER ="Alter";
+	
+	private Integer sourceId;
+	private Integer orderId;
+	private String source;
+	
+	public OrderSource(){
+		
+	}
+	
+	public OrderSource(Integer sourceId, Integer orderId, String source){
+		this.sourceId = sourceId;
+		this.orderId = orderId;
+		this.source = source;
+	}
+	
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name="source_id", unique = true, nullable = false)
+	public Integer getSourceId() {
+		return sourceId;
+	}
+	public void setSourceId(Integer sourceId) {
+		this.sourceId = sourceId;
+	}
+	
+	@Column(name="order_id", nullable = false)
+	public Integer getOrderId() {
+		return orderId;
+	}
+	
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+	
+	@Column(name="source", nullable = true)
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+}
