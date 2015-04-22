e5a545e9d6e9bf55a2c17e9d122fe71fec413147
margine <ch_margine@163.com>
2015-04-22 16:59:57 +0800

    添加了将Date，Timestamp类转换为json可识别的字符串

--- src/main/java/nju/software/util/JSONUtil.java
+++ src/main/java/nju/software/util/JSONUtil.java
@@ -2,10 +2,16 @@ package nju.software.util;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONObject;
+import net.sf.json.JsonConfig;
+import net.sf.json.processors.JsonValueProcessor;
 
 public class JSONUtil {
 	
@@ -15,7 +21,9 @@ public class JSONUtil {
 	 * @param jsonobj
 	 */
 	public void sendJson(HttpServletResponse response, Object object){
-		JSONObject jsonobj = JSONObject.fromObject(object);
+		JsonConfig config = new JsonConfig();
+		config.registerJsonValueProcessor(Timestamp.class, new DateJsonValueProcessor("yyyy-MM-dd HH:mm:ss"));
+		JSONObject jsonobj = JSONObject.fromObject(object, config);
 		response.setContentType("application/json;charset=UTF-8");
 		try {
 			PrintWriter out = response.getWriter();
@@ -27,4 +35,35 @@ public class JSONUtil {
 		}
 	}
 
+	/**
+	 * Date, TimeStamp转换为Json
+	 * @author margine
+	 *
+	 */
+	public class DateJsonValueProcessor implements JsonValueProcessor{
+		public static final String DEAFAULT_DATE_PATTERN = "yyyy-MM-dd";
+		private DateFormat dateFormat;
+
+		public DateJsonValueProcessor(String datePattern){
+			try {
+				dateFormat = new SimpleDateFormat(datePattern);
+			} catch (Exception e) {
+				dateFormat = new SimpleDateFormat(DEAFAULT_DATE_PATTERN);
+			}
+		}
+		@Override
+		public Object processArrayValue(Object value, JsonConfig jsonConfig) {
+			return process(value);
+		}
+
+		@Override
+		public Object processObjectValue(String key, Object value,
+				JsonConfig jsonConfig) {
+			return process(value);
+		}
+		
+		private Object process(Object value){
+			return dateFormat.format((Date)value);
+		}
+	}
 }
